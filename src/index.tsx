173524bff5771ab52ba949276cd50806199e6261
Abdullah Al Fahim <abdullah.fullstack@gmail.com>
2023-03-02 23:58:12 +0600

    INTEGRIGY routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import Router from './Config/Router/Router'
 
 import './index.css'
-import App from './App'
+const container = document.getElementById('root') as Element
+const Root = createRoot(container)
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+Root.render(
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
 )
